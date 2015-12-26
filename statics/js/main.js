fb18ef007c8370209d497bdcf1283da755f1b796
Alexander J. Lallier <alexanderlallier@aol.com>
2015-12-26 18:39:01 -0500

    Implemented Hide Score Check Box
    Implemented the hide score check box functionality. Also fixed up some
    old comments and added a home button easier development.

--- statics/js/main.js
+++ statics/js/main.js
@@ -7,53 +7,129 @@ document.body.style.backgroundColor = '#eee';
 // Automatic Scoring Below
 //This if check is temporary fix, it needs a better solution
 if (window.location.href === "http://localhost:43711/getPlayersNames") {
+  // Event listener to listen all clicks on the page. (Right now handling hideScore and submitRound checkboxes).
+  document.addEventListener('click', function (e) {
+    var checkingHideScore = '';
+
+    // Split the target element so we can check to see what was clicked.
+    hideScoreSplit = e.target.id.split('');
+    
+    // Check to see if hide score check box before going any further. NOTE this is all dependent on id of elements on screen, so be very careful when changing ids in the HTML.
+    for (i = 0; i < 4; i++) {
+      checkingHideScore += hideScoreSplit[i];
+    }
+
+    // If we click a hideScore check box we process this below.
+    if (checkingHideScore === 'hide') {
+
+      // Get the round number from the split.
+      round = hideScoreSplit[9];
+
+      // Clicking a checkbox that is unchecked.
+      if (e.target.value === "false") {
+        for (j = 0; j < nubmerOfPlayers; j++) {
+
+          // Dynamically populate the textBoxName and paragraphName.
+          textBoxName = ['player' + j + 'Round' + round];
+          paragraphName = ['paragrapghPlayer' + j + 'Round' + round];
+
+          // Get the text box input element and parent
+          textBox = document.getElementById(textBoxName);
+          textBoxParentNode = document.getElementById(textBoxName).parentNode;
+
+          // Set the input type to hidden and it's value to true. Thus hiding the input box and setting the check box value to true (checked state).
+          textBox.setAttribute("type", "hidden");
+          e.target.setAttribute("value", "true");
+
+          // Create a paragrpah to hold hidden and give it unquie dunamically created id made from above.
+          paragraph = document.createElement("p");
+          paragraph.setAttribute("id", paragraphName);
+
+          // Creat a text node and give it the value of hidden.
+          node = document.createTextNode("Hidden");
+
+          // Append the node containing hidden to the paragraph and append that paragraph to the textbox parent (which is the table data tag. Thus replacing the input box with the words hidden).
+          paragraph.appendChild(node);
+          textBoxParentNode.appendChild(paragraph);
+        }
+      }
+      // Clicking a checkbox that is checked already.
+      else {
+        for (j = 0; j < nubmerOfPlayers; j++) {
+
+          // Dynamically populate the textBoxName and paragraphName.
+          textBoxName = ['player' + j + 'Round' + round];
+          paragraphName = ['paragrapghPlayer' + j + 'Round' + round];
+
+          // Get the text box input element, text box parent, and paragraph.
+          textBox = document.getElementById(textBoxName);
+          textBoxParentNode = document.getElementById(textBoxName).parentNode;
+          paragraph = document.getElementById(paragraphName);
+
+          // Set the input type to text and it's value to false. Thus re-shwoing the input box and setting the check box value to false (unchecked state).
+          textBox.setAttribute("type", "text");
+          e.target.setAttribute("value", "false");
+
+          // Remove the paragraph so only the text input remains.
+          textBoxParentNode.removeChild(paragraph);
+        }
+      }
+    }
+  });
+  
   var nubmerOfPlayers = document.getElementById('nubmerOfPlayers').value,
       dominoSetSize = document.getElementById('dominoSetSize').value;
 
-  // Add event listeners to all input elements
+  // Add event listeners to all text box input elements.
+  
+  // Loop through all rounds (dependent on dominoe set size).
   for (i = 0; i <= dominoSetSize; i++) {
+    
+    // Loop through number of players.
     for (j = 0; j < nubmerOfPlayers; j++) {
-      test = ['player' + j + 'Round' + i];
-      if (test) {
-        test = document.getElementById(test).addEventListener('input', sumValues);
-      }
+      
+      // Add event listeners to text input boxes.
+      textBoxName = ['player' + j + 'Round' + i];
+      document.getElementById(textBoxName).addEventListener('input', sumValues);
     }
   }
 
-  // Sum values of columns one a time, so to have seperate scores
+  // Sum values of columns one a time, so to have seperate scores.
   function sumValues() {
 
-    // Nested for loops to go through the players and 10 rounds for each player
+    // Loop through number of players.
     for (j = 0; j < nubmerOfPlayers; j++) {
 
-      // Reset total before going into each column
+      // Reset total before going into each column.
       total = 0;
+      
+      // Loop through all rounds (dependent on dominoe set size).
       for (i = 0; i <= dominoSetSize; i++) {
 
-        // Concatinate scoreTextElement before hand so we can loop through unique IDs
+        // Concatinate scoreTextElement before hand so we can loop through unique IDs.
         scoreTextElement = ['player' + j + 'Round' + i];
 
-        // Get a value from each input down a column
+        // Get a value from each input down a column.
         value = Number(document.getElementById(scoreTextElement).value);
 
         total += value;
 
-        // Get the finalScorePlayer element uniquly
+        // Get the finalScorePlayer element uniquly.
         finalScore = ['finalScorePlayer' + j];
         finalScore = document.getElementById(finalScore)
 
-        // Empty finalScorePlayer element so we can rewrite it
+        // Empty finalScorePlayer element so we can rewrite it.
         finalScore.textContent = '';
         finalScore.innerHTML = '',
 
-        // Append new total to finalScorePlayer
+        // Append new total to finalScorePlayer.
         node = document.createTextNode(total);
         finalScore.appendChild(node);
       }
     }
   }
   
-  // Ask the user before they close or refresh the page, so they don't lose their score
+  // Ask the user before they close or refresh the page, so they don't lose their score.
   nodeEnv = document.getElementById('NODE_ENV').value;
 
   window.onbeforeunload = function() {
